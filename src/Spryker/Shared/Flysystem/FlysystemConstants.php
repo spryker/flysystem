b7b7e921fa5d9a16122a35431b026c0c7cf39c10
Oliwier Ptak <oliwier.ptak@spryker.com>
2017-04-21 12:04:16 +0200

    feature/core-1213-filesystem-filemanager moved Flysystem related logic to Flysystem bundle

---
+++ src/Spryker/Shared/Flysystem/FlysystemConstants.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Shared\Flysystem;
+
+interface FlysystemConstants
+{
+
+    const FILESYSTEM_STORAGE = 'FILESYSTEM_STORAGE';
+    const FILESYSTEM_SERVICE = 'FILESYSTEM_SERVICE';
+
+}
